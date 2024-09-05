f3401eddd7df0cdc1f6677a06b49b5d56f8d8dbe
yeonna18k <yeonna18k@gmail.com>
2024-09-05 17:22:34 +0900

    feat: 공통 버튼 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import CommonButton from '@/shared/common/ui/CommonButton';
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24 gap-5">
 			{/* 각자 만든 컴포넌트 불러오기 */}
 			{/* <ExampleComponent />  */}
+			<CommonButton variant="outlined" size="lg" content="생성하기" />
 		</main>
 	);
 }
