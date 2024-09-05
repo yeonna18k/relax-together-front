32e306f082bce57be2f36c676446bb18688af26a
KwonCheulJin <chkftm12@gmail.com>
2024-09-05 17:14:11 +0900

    feat: selectbox 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import SelectBox from '@/shared/common/ui/SelectBox';
+import { selectBoxMenuItems } from '@/shared/fixture/selectbox-menu-items';
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24 gap-5">
 			{/* 각자 만든 컴포넌트 불러오기 */}
 			{/* <ExampleComponent />  */}
+			<SelectBox
+				placeholder="장소를 선택해주세요"
+				menuItems={selectBoxMenuItems}
+			/>
 		</main>
 	);
 }
