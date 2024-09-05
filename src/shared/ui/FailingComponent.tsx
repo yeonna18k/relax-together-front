8f784b49437560ed4d5fb2fdfa60a780f3dca9c0
yeonna18k <yeonna18k@gmail.com>
2024-09-05 13:08:47 +0900

    test: husky test

---
+++ src/shared/ui/FailingComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FailingComponent() {
+  return (
+    <div>
+      <h1> Hello World </h1>
+      <p> This is a component </p>
+    </div>
+  );
+}
