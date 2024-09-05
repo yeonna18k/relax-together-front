bf9db8103d649296c8fbda94fbae69d5feb91ee2
taeyuls <startez03@gmail.com>
2024-09-05 14:07:33 +0900

    test

--- src/shared/ui/FailingComponent.tsx
+++ src/shared/ui/FailingComponent.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function FailingComponent() {
-  return (
-    <div>
-      <h1> Hello World </h1>
-      <p> This is a component </p>
-    </div>
-  );
+	return (
+		<div>
+			<h1> Hello World bye</h1>
+			<p> This is a component </p>
+		</div>
+	);
 }
