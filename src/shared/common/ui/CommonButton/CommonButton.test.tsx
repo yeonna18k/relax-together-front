0b478563f3ab90964c2173adbef40cd3919b8f26
yeonna18k <yeonna18k@gmail.com>
2024-09-06 15:17:58 +0900

    chore: 공통 버튼 렌더링 테스트 추가

---
+++ src/shared/common/ui/CommonButton/CommonButton.test.tsx
@@ -0,0 +1,18 @@
+import CommonButton from '@/shared/common/ui/CommonButton';
+import { render, screen } from '@testing-library/react';
+
+describe('Common Button Component', () => {
+  test('화면에 렌더링이 된다', () => {
+    render(
+      <CommonButton
+        variant="default"
+        size="sm"
+        content="생성하기"
+        onClickHandler={() => console.log('click')}
+      />,
+    );
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent('생성하기');
+    expect(button).toHaveClass('bg-[#ea580c]');
+  });
+});
