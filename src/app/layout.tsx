07a6e334cdcd0b62e1f27e083b7de1826556754e
KwonCheulJin <chkftm12@gmail.com>
2024-09-06 14:00:50 +0900

    feat: 공통 레이아웃 작업중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Context from '@/app/context';
+import Container from '@/shared/layout/Container';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -6,21 +7,23 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: '같이 달램',
-	description:
-		'유저가 바쁜 일상 속 휴식을 위한 다양한 모임을 탐색하고 참여하며, 직접 모임을 개설하고 리뷰를 생성할 수 있는 서비스입니다.',
+  title: '같이 달램',
+  description:
+    '유저가 바쁜 일상 속 휴식을 위한 다양한 모임을 탐색하고 참여하며, 직접 모임을 개설하고 리뷰를 생성할 수 있는 서비스입니다.',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<Context>{children}</Context>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="ko">
+      <body className={`${inter.className} container mx-auto`}>
+        <Context>
+          <Container>{children}</Container>
+        </Context>
+      </body>
+    </html>
+  );
 }
