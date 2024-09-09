152f4d59508890a6d594270f63ad90e8e1d953a5
yeonna18k <yeonna18k@gmail.com>
2024-09-09 13:32:59 +0900

    chore: chip, common-button 테스트 코드 수정

--- src/shared/common/ui/chip/Chip.test.tsx
+++ src/shared/common/ui/chip/Chip.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Chip from '.';
 
 describe('Chip Component', () => {
-  test('화면에 렌더링이 된다', () => {
+  test('화면에 Chip 컴포넌트가 렌더링 된다', () => {
     render(<Chip selected={true}>전체</Chip>);
 
     const chip = screen.getByTestId('chip');
--- src/shared/common/ui/common-button/CommonButton.test.tsx
+++ src/shared/common/ui/common-button/CommonButton.test.tsx
@@ -2,7 +2,7 @@ import CommonButton from '@/shared/common/ui/common-button/index';
 import { render, screen } from '@testing-library/react';
 
 describe('Common Button Component', () => {
-  test('화면에 렌더링이 된다', () => {
+  test('화면에 Common Button 컴포넌트가 렌더링 된다', () => {
     render(
       <CommonButton
         variant="default"
@@ -12,6 +12,7 @@ describe('Common Button Component', () => {
         생성하기
       </CommonButton>,
     );
+
     const button = screen.getByTestId('common-button');
     expect(button).toHaveTextContent('생성하기');
     expect(button).toHaveClass('bg-[#ea580c]');
