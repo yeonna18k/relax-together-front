69e51bc9d943e64fff72edc22d8a110d606fdbf2
KwonCheulJin <chkftm12@gmail.com>
2024-09-09 10:39:48 +0900

    fix: package.json scripts pre-commit에서 린트 및 빌드 검사까지 하도록 수정

--- src/shared/common/ui/tag-clock/index.tsx
+++ src/shared/common/ui/tag-clock/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface TagClockProps {
   message: string;
   variant?: 'default' | 'rounded';
@@ -13,10 +15,13 @@ export default function TagClock({
         data-testid="tag-clock"
         className={`top-[25px] flex min-h-[32px] w-auto items-center gap-2 bg-orange-600 p-[4px_10px_4px_8px] ${variant === 'rounded' ? 'rounded-bl-[12px] rounded-tr-3xl shadow-md' : 'rounded-bl-[12px]'}`}
       >
-        <img
+        <Image
+          priority
           src="/assets/tag-clock.svg"
           alt="Clock Icon"
-          className="order-0 h-[24px] w-[24px] grow-0"
+          className="order-0 grow-0"
+          width={24}
+          height={24}
         />
         <div className="w-auto text-[10px] font-medium leading-[12px] text-white sm:text-xs">
           {message}
--- src/shared/fixture/selectbox-menu-items.ts
+++ src/shared/fixture/selectbox-menu-items.ts
@@ -1,20 +1,20 @@
-import { SelectBoxMenuItem } from '@/shared/common/ui/SelectBox';
+import { SelectBoxMenuItem } from '@/shared/common/ui/select';
 
 export const selectBoxMenuItems: Array<SelectBoxMenuItem> = [
-	{
-		value: '건대입구',
-		label: '건대입구',
-	},
-	{
-		value: '을지로 3가',
-		label: '을지로 3가',
-	},
-	{
-		value: '신림',
-		label: '신림',
-	},
-	{
-		value: '홍대입구',
-		label: '홍대입구',
-	},
+  {
+    value: '건대입구',
+    label: '건대입구',
+  },
+  {
+    value: '을지로 3가',
+    label: '을지로 3가',
+  },
+  {
+    value: '신림',
+    label: '신림',
+  },
+  {
+    value: '홍대입구',
+    label: '홍대입구',
+  },
 ];
