6a5b1da9e252341177838d8eab87fd0e48da3cd5
KwonCheulJin <chkftm12@gmail.com>
2024-09-10 14:57:38 +0900

    chore: localhost 작업 에러 sentry 알림 받지 않기 위한 세팅 수정

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,7 +6,16 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: 'https://854d6e88d4c9ec4747239c9a04cc376b@o4507882123558912.ingest.us.sentry.io/4507893461876736',
-  environment: 'production',
+  beforeSend(event) {
+    if (
+      window.location.hostname === 'localhost' ||
+      window.location.hostname === '127.0.0.1'
+    ) {
+      return null; // 로컬호스트에서 발생한 이벤트는 전송하지 않음
+    }
+    return event;
+  },
+  environment: process.env.NODE_ENV,
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
 
