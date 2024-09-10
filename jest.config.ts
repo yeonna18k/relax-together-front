70f058c43026dc5ef91a94fd45289642e69b10cd
KwonCheulJin <chkftm12@gmail.com>
2024-09-10 11:28:52 +0900

    chore: msw, husky 초기 셋팅으로 변경

--- jest.config.ts
+++ jest.config.ts
@@ -8,29 +8,10 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config: Config = {
-  testEnvironment: 'node',
-  rootDir: __dirname,
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  /**
-   * @note Include the polyfills in the "setupFiles"
-   * to apply them BEFORE the test environment.
-   */
-  setupFiles: ['<rootDir>/jest.polyfills.ts'],
-  transform: {
-    '^.+\\.tsx?$': '@swc/jest',
-  },
-  testEnvironmentOptions: {
-    /**
-     * @note Opt-out from JSDOM using browser-style resolution
-     * for dependencies. This is simply incorrect, as JSDOM is
-     * not a browser, and loading browser-oriented bundles in
-     * Node.js will break things.
-     *
-     * Consider migrating to a more modern test runner if you
-     * don't want to deal with this.
-     */
-    customExportConditions: [''],
-  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
