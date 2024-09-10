006b32538eb3b8288100d41f8dc526012e2505b9
KwonCheulJin <chkftm12@gmail.com>
2024-09-10 11:39:58 +0900

    chore: husky commit-msg, pre-push 검사 추가 chore: msw 세팅 chore: tailwind.config.ts -> content영역 src 전체 파일에 적용되도록 수정

---
+++ commitlint.config.ts
@@ -0,0 +1,17 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'type-enum': [
+      2,
+
+      'always',
+
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+  },
+};
+
+export default Configuration;
---
+++ src/shared/mock/handlers.ts
@@ -0,0 +1,19 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  // Intercept "GET https://example.com/user" requests...
+
+  http.get('https://api.example.com/user', () => {
+    return HttpResponse.json({
+      firstName: 'John',
+
+      lastName: 'Maverick',
+    });
+  }),
+
+  http.get('http://localhost:3000/product', () => {
+    return HttpResponse.json({
+      name: 'Awesome Product',
+    });
+  }),
+];
