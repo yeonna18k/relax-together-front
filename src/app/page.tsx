4ce560f691c9700c01bafd2d5d24b7b7b0f3b5af
KwonCheulJin <chkftm12@gmail.com>
2024-09-10 16:02:27 +0900

    fix: main page 원상 복귀

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-'use client';
-import { Calendar } from '@/shared/ui/calendar/index';
-import { useState } from 'react';
-
 export default function Home() {
-  const [date, setDate] = useState<Date>();
   return (
-    <div className="flex min-h-screen flex-col items-center gap-5 p-24 text-black">
-      <Calendar mode="single" selected={date} onSelect={setDate} />
+    <div className="flex min-h-screen flex-col items-center gap-5 p-24">
+      메인 페이지
     </div>
   );
 }
