468e49fcd7ec9be7c7d4e7b99b1146aa002596cf
KwonCheulJin <chkftm12@gmail.com>
2024-09-11 15:22:58 +0900

    chore: main page 원상 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-'use client';
-import CommonSelect from '@/shared/common/ui/common-select';
-import { commonSelectItems } from '@/shared/fixture/select-items';
-import { useState } from 'react';
-
 export default function Home() {
-  const [selectedValue, setSelectedValue] = useState<string>('ALL');
-
-  const handleValueChange = (value: string) => {
-    setSelectedValue(value);
-  };
   return (
     <div className="flex min-h-screen flex-col items-center gap-5 p-24">
       메인 페이지
-      <CommonSelect
-        filterIconType="default"
-        placeholder="지역 전체"
-        menuItems={commonSelectItems}
-        onValueChange={handleValueChange}
-        selectedValue={selectedValue}
-      />
     </div>
   );
 }
