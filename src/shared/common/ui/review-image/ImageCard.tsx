2b0f7c6f6a43ba29583db0151c43b086d8333d5b
yeonna18k <yeonna18k@gmail.com>
2024-09-11 16:51:55 +0900

    chore: review-card 하위 컴포넌트 파일 위치 정리(shared->features)

--- src/shared/common/ui/review-image/ImageCard.tsx
+++ src/shared/common/ui/review-image/ImageCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-export default function ReviewImage() {
+export default function ImageCard() {
   return (
     <div className="h-[156px] w-[311px] flex-shrink-0 rounded-3xl bg-slate-600 md:w-[280px]">
       <Image src="" alt="" />
