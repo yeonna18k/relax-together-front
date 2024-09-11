fdfcc0be0feab975f6a7e4ae2c84d098ef5c6cf7
yeonna18k <yeonna18k@gmail.com>
2024-09-11 16:40:35 +0900

    feat: review card component 테스트 추가

--- src/shared/common/ui/review-content/HeartImage.tsx
+++ src/shared/common/ui/review-content/HeartImage.tsx
@@ -2,13 +2,11 @@ import Image from 'next/image';
 
 interface HeartImageProps {
   value: number;
-  key: number;
 }
 
-export default function HeartImage({ value, key }: HeartImageProps) {
+export default function HeartImage({ value }: HeartImageProps) {
   return (
     <Image
-      key={key}
       src={value ? '/assets/heart-active.svg' : '/assets/heart-default.svg'}
       alt={value ? 'active-heart' : 'default-heart'}
       width={24}
