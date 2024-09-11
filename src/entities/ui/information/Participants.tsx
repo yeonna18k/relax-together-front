05c9fb3703d9fa8de9993aa984c31a161daa3873
hhyunn <cha9164@naver.com>
2024-09-11 14:10:40 +0900

    refactor: informationBottom 컴포넌트로 분리

---
+++ src/entities/ui/information/Participants.tsx
@@ -0,0 +1,33 @@
+import OpenBadge from '@/shared/common/ui/open-badge';
+import { GatheringsInfoTypes, ParticipantListTypes } from '.';
+import UserProfileImage from './UserProfileImage';
+
+interface ParticipantsProps {
+  gatheringsInfo: GatheringsInfoTypes;
+  participantList: ParticipantListTypes[];
+}
+
+export default function Participants({
+  gatheringsInfo,
+  participantList,
+}: ParticipantsProps) {
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-3 text-sm font-semibold text-gray-900">
+        <div>
+          <span>모집 정원</span>
+          <span data-testid="participantCount" className="ml-[6px]">
+            {gatheringsInfo.participantCount}명
+          </span>
+        </div>
+        <UserProfileImage
+          gatheringsInfo={gatheringsInfo}
+          participantList={participantList}
+        />
+      </div>
+      {gatheringsInfo.participantCount >= 5 ? (
+        <OpenBadge value={gatheringsInfo.participantCount} />
+      ) : null}
+    </div>
+  );
+}
