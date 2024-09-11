09bf9e51d5dbd36239efeb68ae700401fd12f2af
hhyunn <cha9164@naver.com>
2024-09-11 14:18:40 +0900

    refactor: informationTop 컴포넌트 분리

---
+++ src/entities/ui/information/GatheringChip.tsx
@@ -0,0 +1,20 @@
+import ChipInfo from '@/shared/common/ui/chip-info';
+import { formatDate, formatTime } from '@/shared/lib/utils';
+import { GatheringsInfoTypes } from '.';
+
+interface GatheringChipProps {
+  gatheringsInfo: GatheringsInfoTypes;
+}
+
+export default function GatheringChip({ gatheringsInfo }: GatheringChipProps) {
+  // 날짜, 시간 포맷팅
+  const formattedDate = formatDate(gatheringsInfo.dateTime);
+  const formattedTime = formatTime(gatheringsInfo.dateTime);
+
+  return (
+    <div className="mt-3 space-x-2">
+      <ChipInfo type="date">{formattedDate}</ChipInfo>
+      <ChipInfo type="time">{formattedTime}</ChipInfo>
+    </div>
+  );
+}
--- src/entities/ui/information/InformationTop.tsx
+++ src/entities/ui/information/InformationTop.tsx
@@ -1,6 +1,6 @@
-import ChipInfo from '@/shared/common/ui/chip-info';
-import { formatDate, formatTime } from '@/shared/lib/utils';
 import { GatheringsInfoTypes } from '.';
+import GatheringChip from './GatheringChip';
+import GatheringInfo from './GatheringInfo';
 
 interface InformationTopProps {
   gatheringsInfo: GatheringsInfoTypes;
@@ -9,27 +9,13 @@ interface InformationTopProps {
 export default function InformationTop({
   gatheringsInfo,
 }: InformationTopProps) {
-  // 날짜, 시간 포맷팅
-  const formattedDate = formatDate(gatheringsInfo.dateTime);
-  const formattedTime = formatTime(gatheringsInfo.dateTime);
-
   return (
     <div className="p-6 pb-[43px]">
       <div className="flex justify-between gap-4">
-        <div>
-          <h2 className="text-lg font-semibold text-gray-900">
-            {gatheringsInfo.name}
-          </h2>
-          <p className="mt-[2px] text-sm font-medium text-gray-700">
-            {gatheringsInfo.location}
-          </p>
-        </div>
-        <div className="h-12 w-12 shrink-0 bg-sky-500">찜</div>
+        <GatheringInfo gatheringsInfo={gatheringsInfo} />
+        <div className="h-12 w-12 shrink-0 bg-sky-500">찜 컴포넌트</div>
       </div>
-      <div className="mt-3 space-x-2">
-        <ChipInfo type="date">{formattedDate}</ChipInfo>
-        <ChipInfo type="time">{formattedTime}</ChipInfo>
-      </div>
+      <GatheringChip gatheringsInfo={gatheringsInfo} />
     </div>
   );
 }
