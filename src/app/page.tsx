d137e35123a8ce8c8aa18283e37bb78c90032276
KwonCheulJin <chkftm12@gmail.com>
2024-09-12 13:20:52 +0900

    test: byeButton 렌더링 테스트 완료, 반응형 테스트는 storybook으로 대체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import ByeButton from '@/shared/common/ui/bye-button';
-
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center gap-5 bg-black p-24">
       메인 페이지
-      <ByeButton />
     </div>
   );
 }
--- src/shared/common/ui/bye-button/index.tsx
+++ src/shared/common/ui/bye-button/index.tsx
@@ -5,9 +5,9 @@ import { Button } from '@/shared/ui/button';
 export default function ByeButton() {
   //NOTE: 모임 취소 api랑 연동해서 모임 취소하는 기능 추가
   return (
-    <Button className="flex h-9 w-[116px] items-center justify-between rounded-xl border-orange-50 bg-orange-50 px-3 py-1.5 hover:bg-orange-50 active:bg-orange-50 md:h-12 md:w-12 md:rounded-full md:p-3">
+    <Button className="flex h-9 w-[116px] items-center justify-between rounded-xl border-orange-50 bg-orange-50 px-3 py-1.5 hover:bg-orange-50 active:bg-orange-50 xs:h-12 xs:w-12 xs:rounded-full xs:p-3">
       <ByeIcon className="h-6 w-6" />
-      <p className="block text-xs font-semibold text-orange-600 md:hidden">
+      <p className="block text-xs font-semibold text-orange-600 xs:hidden">
         모임 보내주기
       </p>
     </Button>
