6bd540af0524fbd8a497f79bf24021f83521b836
hhyunn <cha9164@naver.com>
2024-09-13 14:25:47 +0900

    feat: pagination 컴포넌트 스토리북 추가

---
+++ src/features/pagination/Pagination.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import Pagination, { PaginationComponentProps } from './index';
+
+const meta = {
+  title: 'Shared/ui/Pagination',
+  component: Pagination,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    currentPage: { control: 'number' },
+    totalPages: { control: 'number' },
+  },
+} satisfies Meta<typeof Pagination>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const Template = (args: PaginationComponentProps) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  return (
+    <Pagination
+      {...args}
+      currentPage={currentPage}
+      setCurrentPage={page => {
+        setCurrentPage(page);
+      }}
+    />
+  );
+};
+
+export const Default: Story = {
+  render: args => <Template {...args} />,
+  args: {
+    reviewList: [],
+    currentPage: 1,
+    setCurrentPage: () => {},
+    totalPages: 9,
+    getReviewData: (page: number) => {},
+  },
+};
