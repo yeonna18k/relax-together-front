d0368a2a3a91e1eeb7605edda54421c405593aff
hhyunn <cha9164@naver.com>
2024-09-13 11:34:28 +0900

    feat: pagination 컴포넌트 생성

---
+++ src/features/pagination/index.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import { cn } from '@/shared/lib/utils';
+import { Pagination, PaginationItemType } from '@nextui-org/pagination';
+import Image from 'next/image';
+import { Dispatch, SetStateAction } from 'react';
+import ArrowLeftIcon from '../../shared/assets/icons/arrow-left-pagination';
+import ArrowRightIcon from '../../shared/assets/icons/arrow-right-pagination';
+import { Review } from './types';
+
+interface PaginationComponentProps {
+  reviewList: Review[];
+  currentPage: number;
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+  totalPages: number;
+  getReviewData: (page: number) => void;
+}
+
+export default function PaginationComponent({
+  reviewList,
+  currentPage,
+  setCurrentPage,
+  totalPages,
+  getReviewData,
+}: PaginationComponentProps) {
+  const handlePageChange = (page: number) => {
+    // 유효한 페이지 내에서만 작동하도록 설정
+    if (page < 1 || page > totalPages) return;
+
+    setCurrentPage(page);
+    getReviewData(currentPage);
+  };
+
+  const renderItem = ({
+    ref,
+    key,
+    value,
+    isActive,
+    onNext,
+    onPrevious,
+    setPage,
+    className,
+  }: any) => {
+    // 첫 페이지, 마지막 페이지일 경우 좌우 버튼 비활성화
+    const isNextDisabled = currentPage === totalPages;
+    const isPrevDisabled = currentPage === 1;
+
+    const handleNextBtnClick = () => {
+      if (!isNextDisabled) {
+        onNext();
+        handlePageChange(currentPage + 1);
+      }
+    };
+
+    const handlePrevBtnClick = () => {
+      if (!isPrevDisabled) {
+        onPrevious();
+        handlePageChange(currentPage - 1);
+      }
+    };
+
+    const handlePageBtnClick = () => {
+      setPage(value);
+      handlePageChange(value);
+    };
+
+    if (value === PaginationItemType.NEXT) {
+      return (
+        <button
+          key={key}
+          className={cn(
+            className,
+            'ml-[6px] h-12 w-12 min-w-12 rounded-lg bg-white',
+          )}
+          onClick={handleNextBtnClick}
+          disabled={isNextDisabled}
+        >
+          <ArrowRightIcon color={isNextDisabled ? '#E5E7EB' : '#1f2937'} />
+        </button>
+      );
+    }
+
+    if (value === PaginationItemType.PREV) {
+      return (
+        <button
+          key={key}
+          className={cn(
+            className,
+            'mr-[6px] h-12 w-12 min-w-12 rounded-lg bg-white',
+          )}
+          onClick={handlePrevBtnClick}
+          disabled={isPrevDisabled}
+        >
+          <ArrowLeftIcon color={isPrevDisabled ? '#E5E7EB' : '#1f2937'} />
+        </button>
+      );
+    }
+
+    if (value === PaginationItemType.DOTS) {
+      return (
+        <button
+          key={key}
+          className={cn(className, 'h-12 w-12 min-w-12 rounded-lg bg-white')}
+        >
+          <Image
+            src="./assets/ellipsis.svg"
+            alt="생략 아이콘 이미지"
+            width={13}
+            height={3}
+          />
+        </button>
+      );
+    }
+
+    return (
+      <button
+        key={key}
+        ref={ref}
+        className={cn(
+          className,
+          'h-12 w-12 min-w-12 rounded-lg bg-white text-[#C4C4C4]',
+          isActive && 'font-semibold text-[#1F1F1F]',
+        )}
+        onClick={handlePageBtnClick}
+      >
+        {value}
+      </button>
+    );
+  };
+
+  return (
+    <>
+      <ul>
+        {/* 임시 key값 index */}
+        {reviewList?.map((review: Review, index) => {
+          return <li key={index}></li>;
+        })}
+      </ul>
+      <Pagination
+        disableCursorAnimation
+        showControls
+        total={totalPages}
+        initialPage={1}
+        className="gap-2"
+        radius="full"
+        renderItem={renderItem}
+        variant="light"
+      />
+    </>
+  );
+}
