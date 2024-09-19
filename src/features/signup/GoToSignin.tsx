2949e789da2ac330eb0244f9841eab878e478ae2
yeonna18k <yeonna18k@gmail.com>
2024-09-19 17:26:08 +0900

    feat: gotosignin font size 수정

--- src/features/signup/GoToSignin.tsx
+++ src/features/signup/GoToSignin.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function GoToSignin() {
   return (
-    <div className="flex justify-center gap-1 font-medium">
+    <div className="flex justify-center gap-1 text-[15px] font-medium">
       <p className="text-gray-800">이미 회원이신가요?</p>
       <Link href="/signin" className="text-green-500 underline">
         로그인
