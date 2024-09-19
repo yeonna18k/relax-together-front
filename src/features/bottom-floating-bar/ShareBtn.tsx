06dd620541e8159f3e0bd4812ed5cac9afe778ab
hhyunn <cha9164@naver.com>
2024-09-19 18:01:26 +0900

    refactor: copyToClipboard 함수 utils 파일에 분리

--- src/features/bottom-floating-bar/ShareBtn.tsx
+++ src/features/bottom-floating-bar/ShareBtn.tsx
@@ -1,22 +1,16 @@
 'use client';
 
 import CommonButton from '@/shared/common/ui/common-button';
+import { copyToClipboard } from '@/shared/lib/utils';
 import { useParams } from 'next/navigation';
 
 export default function ShareBtn() {
   const params = useParams() as { id: string };
 
-  const copyToClipboard = async (text: string) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      alert('링크가 복사되었습니다.');
-    } catch (err) {
-      console.error('클립보드 복사 실패:', err);
-    }
-  };
-
   const handleShareBtnClick = () => {
     const textToCopy = `${process.env.NEXT_PUBLIC_BASE_URL}/gatherings/${params.id}`;
+
+    // 클립보드에 URL 복사
     copyToClipboard(textToCopy);
   };
 
