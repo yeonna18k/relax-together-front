8f85b8a09cfc014b4f44d97eed32c1806bbdc41d
taeyuls <startez03@gmail.com>
2024-09-20 20:50:20 +0900

    fix: 모임 찾기 페이지 반응형 수정 진행중

--- src/app/gatherings/page.tsx
+++ src/app/gatherings/page.tsx
@@ -7,10 +7,10 @@ import SearchFilter from '@/entities/gatherings/ui/main/SearchFilter';
 
 export default function Gatherings() {
   return (
-    <div className="relative flex w-full flex-col justify-center">
+    <div className="relative flex w-full flex-col justify-center px-6">
       <div className="absolute left-0 top-0 z-20 hidden h-[635px] w-full bg-[url('/assets/gathering-no-bg.png')] bg-contain bg-center bg-no-repeat lg:block" />
       <Banner />
-      <div className="z-10 flex h-screen w-full flex-col items-center bg-white">
+      <div className="z-10 flex h-screen w-full flex-col items-center bg-white sm:bg-gray-50 lg:bg-white">
         <GatheringSearch />
         <SearchFilter />
       </div>
