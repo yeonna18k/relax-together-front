6ce300ba00cf0b669a2d4d6febdbe1998dd0708f
KwonCheulJin <chkftm12@gmail.com>
2024-09-20 13:15:53 +0900

    fix: console.log 삭제

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -7,6 +7,6 @@ export default function Mypage() {
   //NOTE: msw api mock 및 useQuery 사용 예시
   useMswApiMock();
   const { user, isLoading } = useUserInfo();
-  console.log('🚀 ~ Mypage ~ { user, isLoading }:', { user, isLoading });
+
   return <div>마이페이지</div>;
 }
