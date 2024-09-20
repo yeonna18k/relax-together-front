c677f1983c281997fdbe9c1ba2d840d1816f803b
KwonCheulJin <chkftm12@gmail.com>
2024-09-20 13:14:36 +0900

    chore: apiService setting, tanstack-query 기본 구조 세팅 chore: msw 개발서버에서 api mocking setting

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import useUserInfo from '@/entities/mypage/api';
+import useMswApiMock from '@/shared/hooks/useMswApiMock';
+
 export default function Mypage() {
+  //NOTE: msw api mock 및 useQuery 사용 예시
+  useMswApiMock();
+  const { user, isLoading } = useUserInfo();
+  console.log('🚀 ~ Mypage ~ { user, isLoading }:', { user, isLoading });
   return <div>마이페이지</div>;
 }
