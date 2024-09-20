c349275b9ff2f57a965e3b279abc237d6438c194
KwonCheulJin <chkftm12@gmail.com>
2024-09-20 17:21:38 +0900

    test: storybook msw mocking test 완료

--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -1,13 +1,9 @@
 import { dummyUser } from '@/shared/fixture/user';
 import { rest } from 'msw';
 
-const API_BASE_URL = 'http://localhost:3000/api';
-
 const handlers = [
-  rest.get(`${API_BASE_URL}/auths/user`, (req, res, ctx) =>
-    res(ctx.json(dummyUser)),
-  ),
-  rest.post(`${API_BASE_URL}/auth/signup`, (req, res, ctx) =>
+  rest.get(`/api/auths/user`, (req, res, ctx) => res(ctx.json(dummyUser))),
+  rest.post(`/api/auth/signup`, (req, res, ctx) =>
     res(ctx.status(201), ctx.json({ accessToken: 'Access-Token' })),
   ),
 ];
