c8f2b507091117d4ca16a69776e5f32eb5b1a537
KwonCheulJin <chkftm12@gmail.com>
2024-09-23 21:44:08 +0900

    chore: test debug 제거

--- src/entities/mypage/ui/common/MypageImage.test.tsx
+++ src/entities/mypage/ui/common/MypageImage.test.tsx
@@ -3,15 +3,15 @@ import { render, screen } from '@testing-library/react';
 
 describe('MypageImage Component', () => {
   test('image 경로를 주입해주지 않으면 기본 이미지 주소를 가지고 있다.', () => {
-    const { debug } = render(<MypageImage />);
-    debug();
+    render(<MypageImage />);
+
     const imageSrc = screen.getByRole('img').getAttribute('src');
     expect(imageSrc).toBe('/assets/default-user.svg');
   });
 
   test('image 경로를 주입하면 해당 이미지를 가지고 있다.', () => {
-    const { debug } = render(<MypageImage image="/assets/user.png" />);
-    debug();
+    render(<MypageImage image="/assets/user.png" />);
+
     const imageSrc = screen.getByRole('img').getAttribute('src');
     expect(imageSrc).toMatch(/user.png/);
   });
