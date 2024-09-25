99c5d17f6a5df3163e8f7392d9661f494d945e73
KwonCheulJin <chkftm12@gmail.com>
2024-09-25 18:21:54 +0900

    fix: 실패 test 수정 작업 완료

--- src/shared/common/ui/review-card/ReviewCard.test.tsx
+++ src/shared/common/ui/review-card/ReviewCard.test.tsx
@@ -1,32 +1,26 @@
+import { myWrittenReviewsContents } from '@/shared/fixture/my-written-reviews';
 import { Page } from '@/shared/lib/constants';
 import { render, screen } from '@testing-library/react';
-import ReviewCard, { ReviewCardProps } from '.';
+import ReviewCard from '.';
 
 describe('Review Card Component', () => {
-  const mockProps: Omit<ReviewCardProps, 'page'> = {
-    score: 3,
-    userName: '럽윈즈올',
-    userProfileImage: '',
-    comment:
-      '따듯하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 이렇게 같이 달램 생기니까 너무 좋아요! 프로그램이 더 많이 늘어났으면 좋겠어요. 따듯하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 이렇게 같이 달램 생기니까 너무 좋아요! 프로그램이 더 많이 늘어났으면 좋겠어요. 따듯하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 이렇게 같이 달램 생기니까 너무 좋아요! 프로그램이 더 많이 늘어났으면 좋겠어요.',
-    gatheringType: '오피스 스트레칭',
-    gatheringLocation: '을지로3가',
-    createdDate: '2024.01.25',
-  };
+  const mockProps = myWrittenReviewsContents[0];
 
   test('화면에 Review Card 컴포넌트의 내용(content)과 별점이 렌더링 된다', () => {
     render(<ReviewCard {...mockProps} page={Page.MYPAGE} />);
 
     const reviewContent = screen.getByText(mockProps.comment);
+    const listItems = screen.getAllByRole('listitem');
+
     expect(reviewContent).toBeInTheDocument();
-    expect(screen.getAllByTestId('active-heart')).toHaveLength(3);
+    expect(listItems).toHaveLength(5);
   });
 
   test('마이페이지에서는 리뷰를 남긴 장소, 날짜가 렌더링 된다', () => {
     render(<ReviewCard {...mockProps} page={Page.MYPAGE} />);
 
     expect(screen.getByText(mockProps.gatheringLocation)).toBeInTheDocument();
-    expect(screen.getByText(mockProps.createdDate)).toBeInTheDocument();
+    expect(screen.getByText('2024. 09. 23.')).toBeInTheDocument();
   });
 
   test('모든 리뷰페이지에서는 리뷰를 남긴 유저의 이름과 장소, 날짜가 렌더링 된다', () => {
@@ -34,13 +28,13 @@ describe('Review Card Component', () => {
 
     expect(screen.getByText(mockProps.userName)).toBeInTheDocument();
     expect(screen.getByText(mockProps.gatheringLocation)).toBeInTheDocument();
-    expect(screen.getByText(mockProps.createdDate)).toBeInTheDocument();
+    expect(screen.getByText('2024. 09. 23.')).toBeInTheDocument();
   });
 
   test('모임 상세페이지에서는 리뷰를 남긴 유저의 이름, 날짜가 렌더링 된다', () => {
     render(<ReviewCard {...mockProps} page={Page.GATHERING_DETAIL} />);
 
     expect(screen.getByText(mockProps.userName)).toBeInTheDocument();
-    expect(screen.getByText(mockProps.createdDate)).toBeInTheDocument();
+    expect(screen.getByText('2024. 09. 23.')).toBeInTheDocument();
   });
 });
