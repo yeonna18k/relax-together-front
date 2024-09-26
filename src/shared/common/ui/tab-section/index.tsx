83340acb7091a6147f20c77484c27f0c3a4c3ea2
KwonCheulJin <chkftm12@gmail.com>
2024-09-26 09:59:17 +0900

    fix: 일부 컴포넌트 수정 작업 중

--- src/shared/common/ui/tab-section/index.tsx
+++ src/shared/common/ui/tab-section/index.tsx
@@ -2,24 +2,29 @@
 
 import SlideTabs from '@/shared/common/ui/tab-section/SlideTabs';
 import { Tab } from '@/shared/common/ui/tab-section/TabItem';
-import { useSearchParams } from 'next/navigation';
+import { cn } from '@/shared/lib/utils';
 
 export interface TabSectionProps {
   tabs: Array<Tab>;
   href: string;
   isView: boolean;
   buttonGroup: React.ReactNode;
+  className?: string;
 }
 export default function TabSection({
   tabs,
   href,
   isView,
   buttonGroup,
+  className,
 }: TabSectionProps) {
-  const searchParams = useSearchParams();
-  const subPage = searchParams.get('subPage');
   return (
-    <section className="flex w-full flex-col gap-6 md:flex-row md:justify-between md:gap-0">
+    <section
+      className={cn(
+        'flex w-full flex-col gap-6 md:flex-row md:justify-between md:gap-0',
+        className,
+      )}
+    >
       <SlideTabs tabs={tabs} href={href} />
       {isView && <>{buttonGroup}</>}
     </section>
--- src/shared/hooks/useResponsiveDevice.ts
+++
@@ -1,9 +0,0 @@
-import { Device } from '@/shared/lib/constants';
-import { useWindowSize } from 'usehooks-ts';
-
-export default function useResponsiveDevice() {
-  const { width } = useWindowSize();
-  const isResponsiveDevice =
-    width === undefined ? false : width < Device.tablet;
-  return isResponsiveDevice;
-}
