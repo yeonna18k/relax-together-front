91f59f1c23687bd7fa6e4674424ec181e2a2edf7
yeonna18k <yeonna18k@gmail.com>
2024-10-01 00:20:21 +0900

    feat: review list 생성, dummyscore 수정

--- src/features/reviews/ui/review-list/index.tsx
+++ src/features/reviews/ui/review-list/index.tsx
@@ -1,3 +1,7 @@
+import { myWrittenReviewsDummyData } from '@/shared/fixture/my-written-reviews';
+
 export default function ReviewList() {
+  const reviews = myWrittenReviewsDummyData.content;
+  console.log(myWrittenReviewsDummyData);
   return <div>ReviewList</div>;
 }
--- src/shared/fixture/score.ts
+++ src/shared/fixture/score.ts
@@ -1,8 +1,6 @@
 import { Score } from '@/features/reviews/model/score';
 
 export const dummyScore: Score = {
-  // gatheringId: 1,
-  // type: '달램핏',
   fivePoints: 27,
   fourPoints: 19,
   threePoints: 2,
