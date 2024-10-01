4d6dc9315b3d11d2603f0d339cc5feb6c1b7d1c4
KwonCheulJin <chkftm12@gmail.com>
2024-10-01 16:09:22 +0900

    chore: ci/cd base url check

--- src/shared/api/service/CommonApiService.ts
+++ src/shared/api/service/CommonApiService.ts
@@ -1,6 +1,7 @@
 import ApiService from '@/shared/api/service/ApiService';
 import { BASE_URL } from '@/shared/lib/constants';
 
+console.log('🚀 ~ CommonApiService ~ leaveGatheringById ~ BASE_URL:', BASE_URL);
 class CommonApiService extends ApiService {
   async leaveGatheringById(gatheringId: number) {
     const response = await this.delete(
