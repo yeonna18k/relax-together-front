09549dcfce2325f9d0a8753f3561f1073c8ba68c
KwonCheulJin <chkftm12@gmail.com>
2024-10-01 16:16:36 +0900

    chore: firebase NEXT_PUBLIC_API_URL settings

--- src/shared/api/service/CommonApiService.ts
+++ src/shared/api/service/CommonApiService.ts
@@ -1,7 +1,6 @@
 import ApiService from '@/shared/api/service/ApiService';
 import { BASE_URL } from '@/shared/lib/constants';
 
-console.log('🚀 ~ CommonApiService ~ leaveGatheringById ~ BASE_URL:', BASE_URL);
 class CommonApiService extends ApiService {
   async leaveGatheringById(gatheringId: number) {
     const response = await this.delete(
