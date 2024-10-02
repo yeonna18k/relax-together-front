4624df44bbfd9d1195d799a9ce9e5e92bf60931c
yeonna18k <yeonna18k@gmail.com>
2024-10-02 18:47:20 +0900

    feat: reviews filtering 로직 변경

--- src/entities/reviews/ui/review-list/index.tsx
+++ src/entities/reviews/ui/review-list/index.tsx
@@ -1,35 +1,16 @@
 'use client';
-import useCommonSearchParams from '@/entities/mypage/model/hooks/useCommonSearchParams';
-import { getCurrentTypeMap } from '@/features/gatherings/ui/card-list-section';
+import useAdditionalParams from '@/features/gatherings/model/hook/useAdditionalParams';
 import ReviewCard from '@/shared/common/ui/review-card';
 import { myWrittenReviewsDummyData } from '@/shared/fixture/my-written-reviews';
-import { useSearchFilter } from '@/shared/hooks/useSearchFilter';
 import { Review } from '@/shared/model';
-import { format } from 'date-fns';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useReviewsData } from '../../api/queries/reviews';
 
 export default function ReviewList() {
   const reviews: Review[] = myWrittenReviewsDummyData.content;
-  const { currentSubPage, currentFilter } = useCommonSearchParams();
-  const { searchFilterValues } = useSearchFilter();
-  const target =
-    currentSubPage === 'workation'
-      ? currentSubPage
-      : `${currentSubPage}_${currentFilter}`;
-  const type = getCurrentTypeMap[target];
-  const { data, fetchNextPage, isFetching } = useReviewsData({
-    type,
-    location:
-      searchFilterValues.selectedValue === 'ALL'
-        ? undefined
-        : searchFilterValues.selectedValue,
-    date: searchFilterValues.date
-      ? format(searchFilterValues.date, 'yyyy-MM-dd')
-      : undefined,
-    sortBy: searchFilterValues.selectedSortValue,
-  });
+  const { additionalParams } = useAdditionalParams();
+  const { data, fetchNextPage, isFetching } = useReviewsData(additionalParams);
   console.log(data);
   const { ref, inView } = useInView();
 
