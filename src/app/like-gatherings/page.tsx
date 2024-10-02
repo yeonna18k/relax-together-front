81fc98eb1206f4362e741421ff8b5eb557f64471
hhyunn <cha9164@naver.com>
2024-10-02 17:01:04 +0900

    fix: build error 수정

--- src/app/like-gatherings/page.tsx
+++ src/app/like-gatherings/page.tsx
@@ -1,11 +1,29 @@
+import { additionalParams } from '@/entities/gatherings/api/queries/gatherings';
+import { fetchLikeGatherings } from '@/entities/like-gatherings/api/like-gatherings';
 import LikeGatheringsBanner from '@/entities/like-gatherings/ui/like-gatherings-banner';
 import LikeGatheringCardList from '@/entities/like-gatherings/ui/like-gatherings-main/LikeGatheringCardList';
 import LikeGatheringsTitle from '@/entities/like-gatherings/ui/like-gatherings-main/LikeGatheringsTitle';
+import { prefetchCommonInfiniteData } from '@/shared/api/queries/prefetch';
 
 import CommonSearchFilter from '@/shared/common/ui/common-search-filter';
 import { gatheringsSortItems } from '@/shared/fixture/select-items';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { Suspense } from 'react';
 
-export default function LikeGatherings() {
+export default async function LikeGatherings() {
+  const queryClient = new QueryClient();
+
+  await prefetchCommonInfiniteData(
+    queryClient,
+    ['like-gatherings', additionalParams],
+    fetchLikeGatherings,
+    additionalParams,
+  );
+
   return (
     <div className="mx-auto max-w-[996px]">
       <LikeGatheringsBanner />
@@ -13,7 +31,11 @@ export default function LikeGatherings() {
         <LikeGatheringsTitle />
         <div className="px-4">
           <CommonSearchFilter sortItems={gatheringsSortItems} />
-          <LikeGatheringCardList />
+          <Suspense fallback={null}>
+            <HydrationBoundary state={dehydrate(queryClient)}>
+              <LikeGatheringCardList />
+            </HydrationBoundary>
+          </Suspense>
         </div>
       </div>
     </div>
