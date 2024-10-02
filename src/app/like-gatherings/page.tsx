e0bb9c563a387a8ad9bd8d2e60c0dc0ebe509236
hhyunn <cha9164@naver.com>
2024-10-02 16:09:37 +0900

    feat: 찜한 모임 페이지 구현

--- src/app/like-gatherings/page.tsx
+++ src/app/like-gatherings/page.tsx
@@ -1,3 +1,21 @@
+import LikeGatheringsBanner from '@/entities/like-gatherings/ui/like-gatherings-banner';
+import LikeGatheringCardList from '@/entities/like-gatherings/ui/like-gatherings-main/LikeGatheringCardList';
+import LikeGatheringsTitle from '@/entities/like-gatherings/ui/like-gatherings-main/LikeGatheringsTitle';
+
+import CommonSearchFilter from '@/shared/common/ui/common-search-filter';
+import { gatheringsSortItems } from '@/shared/fixture/select-items';
+
 export default function LikeGatherings() {
-  return <div>찜한 모임 페이지</div>;
+  return (
+    <div className="mx-auto max-w-[996px]">
+      <LikeGatheringsBanner />
+      <div className="sm:px-6 lg:px-0">
+        <LikeGatheringsTitle />
+        <div className="px-4">
+          <CommonSearchFilter sortItems={gatheringsSortItems} />
+          <LikeGatheringCardList />
+        </div>
+      </div>
+    </div>
+  );
 }
