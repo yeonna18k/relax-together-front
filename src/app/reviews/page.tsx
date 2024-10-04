acea34467998f7a7b7b2d57d294262db7d50e095
yeonna18k <yeonna18k@gmail.com>
2024-10-04 16:50:36 +0900

    feat: reviews redirect middleware 처리

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,12 +1,24 @@
+import { additionalParams } from '@/entities/gatherings/api/queries/gatherings';
+import { fetchReviews } from '@/entities/reviews/api/reviews';
 import CategoryTab from '@/entities/reviews/ui/category-tab';
 import ReviewBanner from '@/entities/reviews/ui/review-banner';
 import ReviewList from '@/entities/reviews/ui/review-list';
 import ReviewScore from '@/entities/reviews/ui/review-score';
+import { prefetchCommonInfiniteData } from '@/shared/api/queries/prefetch';
 import CommonSearchFilter from '@/shared/common/ui/common-search-filter';
 import { reviewsSortItems } from '@/shared/fixture/select-items';
 import { Path } from '@/shared/lib/constants';
+import { QueryClient } from '@tanstack/react-query';
 
-export default function Reviews() {
+export default async function Reviews() {
+  const queryClient = new QueryClient();
+  await prefetchCommonInfiniteData(
+    queryClient,
+    ['reviews', additionalParams],
+    fetchReviews,
+    additionalParams,
+  );
+
   return (
     <div className="mx-auto bg-gray-50 lg:max-w-[996px]">
       <ReviewBanner />
--- src/middleware.ts
+++ src/middleware.ts
@@ -32,6 +32,12 @@ export function middleware(req: NextRequest) {
     url.searchParams.set('filter', 'all');
     return NextResponse.redirect(url);
   }
+
+  if (url.pathname === '/reviews' && !url.searchParams.has('subPage')) {
+    url.searchParams.set('subPage', 'dalaemfit');
+    url.searchParams.set('filter', 'all');
+    return NextResponse.redirect(url);
+  }
 }
 
 export const config = {
