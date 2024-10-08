831d0d507b2f7ec97d6ea5973ad41b0bdf16b157
KwonCheulJin <chkftm12@gmail.com>
2024-10-08 10:50:51 +0900

    fix: card title 워케이션, 달램핏 구분해서 표현되도록 수정

--- src/entities/gatherings/ui/card/GatheringCardContentSection.tsx
+++ src/entities/gatherings/ui/card/GatheringCardContentSection.tsx
@@ -4,16 +4,10 @@ import CardTitle from '@/shared/common/ui/card-title';
 import ChipInfo from '@/shared/common/ui/chip-info';
 import LikeButton from '@/shared/common/ui/like-button';
 import { formatDate, formatTime } from '@/shared/lib/utils';
-type GatheringType = '워케이션' | '달램핏' | '오피스 스트레칭' | '마인드풀니스';
 
 interface GatheringCardContentSectionProps
-  extends Omit<GatheringCardProps, 'imageUrl' | 'message'> {
-  // isExpired?: boolean;
-  name: string | null;
-  type: GatheringType;
-}
+  extends Omit<GatheringCardProps, 'imageUrl' | 'message'> {}
 
-// displayType을 string | GatheringType 으로 처리
 export default function GatheringCardContentSection(
   props: GatheringCardContentSectionProps,
 ) {
@@ -27,20 +21,12 @@ export default function GatheringCardContentSection(
     name,
     registrationEnd,
   } = props;
-  // const isExpired = new Date(registrationEnd) < new Date(); // 등록 종료 날짜를 기준으로 계산
-
-  // const cardClassName = isExpired ? 'blur-sm' : '';
-  // 워케이션일 경우 type 대신 name을 사용하도록 처리
-  const displayType: string | GatheringType =
-    type === '워케이션' && name ? name : type;
 
   return (
-    <div
-      className={`$[cardClassName] flex w-full flex-col justify-between gap-5 py-4 sm:w-[calc(100%-280px)]`}
-    >
+    <div className="flex w-full flex-col justify-between gap-5 py-4 sm:w-[calc(100%-280px)]">
       <div className="flex justify-between pl-6 pr-4">
         <div>
-          <CardTitle type={displayType} location={location} />
+          <CardTitle type={type} name={name} location={location} />
           <div className="mt-2 flex items-start justify-start">
             <div className="items-start space-x-2">
               <ChipInfo type="date">{formatDate(dateTime)}</ChipInfo>
@@ -50,8 +36,6 @@ export default function GatheringCardContentSection(
         </div>
         <LikeButton gatheringId={id.toString()} />
       </div>
-
-      {/* Progress Bar */}
       <ProgressBar
         participantCount={participantCount}
         capacity={capacity}
--- src/shared/common/ui/card-title/index.tsx
+++ src/shared/common/ui/card-title/index.tsx
@@ -1,15 +1,16 @@
 import { GatheringType } from '@/shared/model';
 
 interface CardTitleProps {
-  type: string | GatheringType; // string도 받을 수 있게 수정
+  type: GatheringType;
+  name: string | null;
   location: string;
 }
 
-export default function CardTitle({ type, location }: CardTitleProps) {
+export default function CardTitle({ type, name, location }: CardTitleProps) {
   return (
     <div className="mb-1.5 mt-3 flex items-center justify-start gap-2">
       <h2 className="text-xl font-semibold text-gray-800">
-        {type} {/* 워케이션일 때는 name을, 그 외에는 type을 출력 */}
+        {type !== '워케이션' ? `달램핏 ${type}` : `${type} ${name}`}
       </h2>
       <span className="text-lg font-semibold text-gray-900">|</span>
       <p className="text-gray-700">{location}</p>
