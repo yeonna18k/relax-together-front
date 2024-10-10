45c4b3bc30d917903846e554dbc8b78183973d99
yeonna18k <yeonna18k@gmail.com>
2024-10-10 15:07:07 +0900

    feat: 쿼리로 훅 만들어서 score 데이터 페칭, 스코어 로직 수정

--- src/entities/reviews/api/queries/reviews.ts
+++ src/entities/reviews/api/queries/reviews.ts
@@ -1,7 +1,8 @@
 import { FilterParams } from '@/entities/gatherings/model/params';
 import { useCommonInfiniteData } from '@/entities/mypage/api/queries';
 import { Review } from '@/shared/model';
-import { fetchReviews } from '../reviews';
+import { useQuery } from '@tanstack/react-query';
+import { fetchReviews, fetchReviewScore } from '../reviews';
 
 export function useReviewsData(additionalParams: Partial<FilterParams>) {
   return useCommonInfiniteData<Review, Partial<FilterParams>>(
@@ -9,4 +10,12 @@ export function useReviewsData(additionalParams: Partial<FilterParams>) {
     fetchReviews,
     additionalParams,
   );
+}
+
+export function useReviewScoreQuery({ type }: Partial<FilterParams>) {
+  const { data } = useQuery({
+    queryKey: ['reviewScore', type],
+    queryFn: () => fetchReviewScore({ type }),
+  });
+  return { data };
 }
--- src/entities/reviews/ui/review-score/index.tsx
+++ src/entities/reviews/ui/review-score/index.tsx
@@ -2,35 +2,35 @@
 
 import useAdditionalParams from '@/features/gatherings/model/hook/useAdditionalParams';
 import ReviewHearts from '@/shared/common/ui/review-card/ReviewHearts';
-import { dummyScore } from '@/shared/fixture/score';
 import { Progress } from '@/shared/ui/progress';
+import { useReviewScoreQuery } from '../../api/queries/reviews';
 
 export default function ReviewScore() {
   const { additionalParams } = useAdditionalParams();
-  const type = additionalParams.type;
-  const location = additionalParams.location;
+  const { type } = additionalParams;
+  const { data } = useReviewScoreQuery({ type });
 
-  const scoreArray = Array.from(Object.values(dummyScore)).reverse();
+  const scoreArray = data ? Object.values(data) : [];
   const scoreCount = scoreArray.reduce((sum, count) => sum + count, 0);
   const scoreAverage = Math.floor(
     scoreArray.reduce((sum, count, index) => {
-      return sum + count * (index + 1);
+      return sum + count * (scoreArray.length - index);
     }, 0) / scoreCount,
   );
   return (
-    <div className="mt-[10px] flex h-[184px] w-full justify-center border-y-2 border-y-gray-200 bg-white py-8 md:mt-5 lg:mt-6">
+    <div className="mt-2.5 flex h-[184px] w-full justify-center border-y-2 border-y-gray-200 bg-white py-8 md:mt-5 lg:mt-6">
       <div className="flex w-[294px] justify-between gap-6 md:w-[550px] md:gap-[120px] lg:w-[610px] lg:gap-[180px]">
         <div className="flex w-[128px] flex-col items-center justify-center gap-2">
-          <div className="flex gap-[2px] text-xl font-semibold md:text-2xl">
+          <div className="flex gap-0.5 text-xl font-semibold md:text-2xl">
             <span className="text-gray-900">{scoreAverage.toFixed(1)}</span>
             <span className="text-gray-400">/5</span>
           </div>
-          <div className="flex gap-[2px] text-xl font-semibold">
+          <div className="flex gap-0.5 text-xl font-semibold">
             <ReviewHearts score={scoreAverage} />
           </div>
         </div>
         <div className="flex w-[146px] flex-col gap-1 md:w-[302px]">
-          {scoreArray.reverse().map((score, index) => {
+          {scoreArray.map((score, index) => {
             return (
               <div
                 key={`${5 - index}-star-rating`}
@@ -38,7 +38,7 @@ export default function ReviewScore() {
               >
                 <div className="flex w-[117px] items-center justify-between gap-3 md:w-[273px]">
                   <span className="whitespace-nowrap text-gray-700">
-                    {5 - index}점
+                    {scoreArray.length - index}점
                   </span>
                   <Progress
                     capacity={scoreCount}
