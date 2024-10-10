51fc466cd288faaeb82dcd102b0869b0f0470f7f
yeonna18k <yeonna18k@gmail.com>
2024-10-10 16:32:06 +0900

    chore: reviews page suspense 추가

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -9,6 +9,7 @@ import CommonSearchFilter from '@/shared/common/ui/common-search-filter';
 import { reviewsSortItems } from '@/shared/fixture/select-items';
 import { Path } from '@/shared/lib/constants';
 import { QueryClient } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 export default async function Reviews() {
   const queryClient = new QueryClient();
@@ -20,14 +21,19 @@ export default async function Reviews() {
   );
 
   return (
-    <div className="mx-auto bg-gray-50 lg:max-w-[996px]">
-      <ReviewBanner />
-      <div className="px-4 md:px-6">
-        <CategoryTab />
-        <ReviewScore />
-        <CommonSearchFilter sortItems={reviewsSortItems} path={Path.reviews} />
-        <ReviewList />
+    <Suspense fallback={null}>
+      <div className="mx-auto bg-gray-50 lg:max-w-[996px]">
+        <ReviewBanner />
+        <div className="px-4 md:px-6">
+          <CategoryTab />
+          <ReviewScore />
+          <CommonSearchFilter
+            sortItems={reviewsSortItems}
+            path={Path.reviews}
+          />
+          <ReviewList />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
