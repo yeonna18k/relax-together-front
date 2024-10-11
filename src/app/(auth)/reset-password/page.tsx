379201298641a53c5a640274a84699881595d9e6
KwonCheulJin <chkftm12@gmail.com>
2024-10-11 15:59:21 +0900

    fix: 비밀번호 재설정 페이지 코드 정리

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,20 +1,5 @@
-'use client';
-
 import ResetPasswordForm from '@/features/auth/reset-password/ui/ResetPassword';
-import { useModal } from '@/shared/hooks/useModal';
-import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function ResetPasswordPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const { openModal } = useModal();
-  const email = searchParams.get('email');
-
-  return (
-    <div>
-      <ResetPasswordForm />
-      {/* 에러 모달 추가 */}
-      {/* 모달을 클릭하면 화면이동 */}
-    </div>
-  );
+  return <ResetPasswordForm />;
 }
