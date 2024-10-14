5b848c58984f4af9d8000b1ee412dee33569fe6c
hhyunn <cha9164@naver.com>
2024-10-14 16:17:25 +0900

    feat: 공유하기 확인을 alert에서 모달로 변경

--- src/features/bottom-floating-bar/ui/ShareBtn.tsx
+++ src/features/bottom-floating-bar/ui/ShareBtn.tsx
@@ -1,27 +1,46 @@
 'use client';
 
 import CommonButton from '@/shared/common/ui/common-button';
+import Modal from '@/shared/common/ui/modal';
+import { useModal } from '@/shared/hooks/useModal';
 import { copyToClipboard } from '@/shared/lib/utils';
 import { useParams } from 'next/navigation';
 
 export default function ShareBtn() {
   const params = useParams() as { id: string };
 
+  const { modal, openModal, closeModal } = useModal();
+
   const handleShareBtnClick = () => {
     const textToCopy = `${process.env.NEXT_PUBLIC_BASE_URL}/gatherings/${params.id}`;
 
     // 클립보드에 URL 복사
     copyToClipboard(textToCopy);
+
+    openModal('ShareConfirmModal');
+  };
+
+  const handleOnClick = () => {
+    closeModal('ShareConfirmModal');
   };
 
   return (
-    <CommonButton
-      variant="default"
-      size="lg"
-      className="h-11 w-1/2 sm:w-[115px]"
-      onClick={handleShareBtnClick}
-    >
-      공유하기
-    </CommonButton>
+    <>
+      <CommonButton
+        variant="default"
+        size="lg"
+        className="h-11 w-1/2 sm:w-[115px]"
+        onClick={handleShareBtnClick}
+      >
+        공유하기
+      </CommonButton>
+      {modal.includes('ShareConfirmModal') && (
+        <Modal variant="notice" size="sm" handleAction={handleOnClick}>
+          <p className="text-center text-base font-medium text-[#111827]">
+            링크가 복사되었습니다.
+          </p>
+        </Modal>
+      )}
+    </>
   );
 }
