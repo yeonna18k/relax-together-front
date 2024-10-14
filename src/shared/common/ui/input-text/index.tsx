3ff8343db71474ceeb3c5aa14e80bcf8455c4992
KwonCheulJin <chkftm12@gmail.com>
2024-10-14 09:23:13 +0900

    feat: 리뷰 작성 글 제한 기능 추가

--- src/shared/common/ui/input-text/index.tsx
+++ src/shared/common/ui/input-text/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MAX_REVIEW_TEXT } from '@/shared/lib/constants';
+import { MAX_REVIEW_TEXT_COUNT } from '@/shared/lib/constants';
 import { Textarea, TextareaProps } from '@/shared/ui/textarea';
 import { ChangeEvent, useState } from 'react';
 
@@ -17,12 +17,12 @@ export default function InputText({
 }: InputTextProps) {
   const [textCount, setTextCount] = useState(0);
   const handleOnChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    if (e.target.value.length > MAX_REVIEW_TEXT) return;
+    if (e.target.value.length > MAX_REVIEW_TEXT_COUNT) return;
     setValue(e.target.value);
     setTextCount(e.target.value.length);
   };
   const textColor =
-    textCount >= MAX_REVIEW_TEXT ? 'text-red-500' : 'text-gray-300';
+    textCount >= MAX_REVIEW_TEXT_COUNT ? 'text-red-400' : 'text-gray-400';
   return (
     <div className="relative rounded-xl bg-gray-50 pb-2">
       <Textarea
@@ -30,8 +30,8 @@ export default function InputText({
         value={value}
         onChange={handleOnChange}
       />
-      <p className={`absolute bottom-0 right-2 ${textColor}`}>
-        {textCount} / {MAX_REVIEW_TEXT}자
+      <p className={`absolute bottom-2 right-4 text-sm ${textColor}`}>
+        ({textCount}/{MAX_REVIEW_TEXT_COUNT})
       </p>
     </div>
   );
