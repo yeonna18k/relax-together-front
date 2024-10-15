4e588e9db7e2fe85626af13c85848fb3eaaabc83
yeonna18k <yeonna18k@gmail.com>
2024-10-15 10:29:23 +0900

    feat: 이메일 인증 모달 문구 수정

--- src/features/auth/signup/ui/EmailAuthModal.tsx
+++ src/features/auth/signup/ui/EmailAuthModal.tsx
@@ -18,12 +18,16 @@ export default function EmailAuthModal({
       disabled={otp.length < 6}
       handleAction={() => handleVerifyCode(otp)}
     >
-      <div className="flex flex-col items-center gap-6">
+      <div className="flex flex-col items-center gap-2">
         <p className="text-gray-800">
           이메일로 전송된 인증번호 6자리를 입력해주세요
         </p>
+        <p className="text-center text-xs text-gray-600">
+          메일을 받지 못하신 경우 스팸 메일함을 확인해주세요
+          <br />이 창을 벗어나면 인증번호가 만료됩니다
+        </p>
         <InputOTP maxLength={6} onChange={e => setOtp(e)}>
-          <InputOTPGroup>
+          <InputOTPGroup className="my-4">
             <InputOTPSlot index={0} />
             <InputOTPSlot index={1} />
             <InputOTPSlot index={2} />
