089a5c666f86367344a58187403f5e158f231daa
KwonCheulJin <chkftm12@gmail.com>
2024-10-16 17:38:58 +0900

    chore: playwright.yml 수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,16 +52,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
     {
       name: 'userSetup',
       testMatch: '**/*.user.setup.ts',
