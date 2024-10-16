09ccf64897d1ae9a6967f760236f1bf4102ffa2e
KwonCheulJin <chkftm12@gmail.com>
2024-10-16 15:14:34 +0900

    chore: e2e test 검사 제외 script 작성-4

--- scripts/pre-push-check.sh
+++ scripts/pre-push-check.sh
@@ -3,17 +3,17 @@
 # 변경된 파일 목록 가져오기 (Playwright 테스트 파일 제외)
 changed_files=$(git diff --name-only --cached | grep -v "\.spec\.ts$")
 
-# 모든 .test.ts 파일 목록 가져오기 (서브디렉토리 포함)
-test_files=$(find . -type f -name "*.test.ts")
+# src 디렉토리 안의 모든 .test.ts 파일 목록 가져오기 (node_modules 폴더 제외)
+test_files=$(find ./src -type f -name "*.test.ts" -not -path "*/node_modules/*")
 
-# 변경된 파일과 모든 테스트 파일 합치기
+# 변경된 파일과 src 폴더 내의 테스트 파일 합치기
 all_files="$changed_files $test_files"
 
 # 변경된 파일이나 테스트 파일이 있는지 확인
 if [ -n "$all_files" ]; then
-    echo "Running tests on changed and all test files..."
+    echo "Running tests on changed and src test files..."
 
-    # Jest를 사용하여 변경된 파일과 모든 테스트 파일에 대해서 테스트 실행
+    # Jest를 사용하여 변경된 파일과 모든 src 폴더 내의 테스트 파일에 대해서 테스트 실행
     if ! jest --findRelatedTests $all_files; then
         echo "Pre-push checks failed: Jest tests failed"
         exit 1
