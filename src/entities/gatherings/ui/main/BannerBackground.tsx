eb46293a32177afd70f7e5e138689a0a7c1d6378
KwonCheulJin <chkftm12@gmail.com>
2024-10-17 17:25:17 +0900

    chore: svg 큰 이미지 firebase storage에서 받아오는 로직으로 수정

--- src/entities/gatherings/ui/main/BannerBackground.tsx
+++ src/entities/gatherings/ui/main/BannerBackground.tsx
@@ -5,7 +5,7 @@ export default function BannerBackground() {
     <div className="absolute left-0 top-0 z-20 hidden h-[635px] w-full xl:flex xl:justify-center">
       <Image
         className="h-auto w-full object-cover"
-        src="/assets/gathering-no-bg.png"
+        src="https://firebasestorage.googleapis.com/v0/b/relax-together.appspot.com/o/images%2Fgathering-no-bg.png?alt=media&token=002db5da-62ef-48fa-9667-bfd0113c342c"
         alt="Background image"
         width={1200}
         height={635}
