4c177df1ad92aa01a97b2df659f7f43e6629332a
hhyunn <cha9164@naver.com>
2024-10-30 19:08:26 +0900

    chore: pagination 스토리북 수정

--- src/features/pagination-reviews/ui/PaginationReviews.stories.tsx
+++ src/features/pagination-reviews/ui/PaginationReviews.stories.tsx
@@ -1,10 +1,12 @@
+import { dummyReviews } from '@/shared/fixture/reviews';
+import { REVIEWS_PER_PAGE } from '@/shared/lib/constants';
 import type { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
-import Pagination, { PaginationReviewsProps } from './index';
+import PaginationReviews, { PaginationReviewsProps } from '.';
 
 const meta = {
-  title: 'Features/ui/Pagination',
-  component: Pagination,
+  title: 'Features/ui/PaginationReviews',
+  component: PaginationReviews,
   parameters: {
     layout: 'centered',
   },
@@ -13,22 +15,29 @@ const meta = {
     currentPage: { control: 'number' },
     totalPages: { control: 'number' },
   },
-} satisfies Meta<typeof Pagination>;
+} satisfies Meta<typeof PaginationReviews>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 const Template = (args: PaginationReviewsProps) => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(args.currentPage || 0);
+
+  const start = currentPage * REVIEWS_PER_PAGE;
+  const end = start + REVIEWS_PER_PAGE;
+
+  const paginatedReviews = {
+    ...dummyReviews,
+    reviews: dummyReviews.reviews.slice(start, end),
+  };
 
   return (
-    <Pagination
+    <PaginationReviews
       {...args}
+      reviewList={paginatedReviews}
       currentPage={currentPage}
-      setCurrentPage={page => {
-        setCurrentPage(page);
-      }}
+      setCurrentPage={page => setCurrentPage(page)}
     />
   );
 };
@@ -36,14 +45,9 @@ const Template = (args: PaginationReviewsProps) => {
 export const Default: Story = {
   render: args => <Template {...args} />,
   args: {
-    reviewList: {
-      reviews: [],
-      currentPage: 1,
-      totalPages: 0,
-      totalElements: 0,
-    },
-    currentPage: 1,
+    reviewList: dummyReviews,
+    currentPage: 0,
     setCurrentPage: () => {},
-    totalPages: 9,
+    totalPages: Math.ceil(dummyReviews.totalElements / REVIEWS_PER_PAGE),
   },
 };
